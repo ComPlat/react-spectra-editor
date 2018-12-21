aae0d57d1c1f529313c34434bcb3ee0e681300bd
jasonych99 <jasonych99@gmail.com>
2018-12-21 13:31:04 +0100

    add Manual

---
+++ src/components/panel/manual.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+import { withStyles } from '@material-ui/core/styles';
+import InfoIcon from '@material-ui/icons/Info';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Divider from '@material-ui/core/Divider';
+import Card from '@material-ui/core/Card';
+import Typography from '@material-ui/core/Typography';
+
+const styles = {
+  root: {
+  },
+  avatar: {
+    margin: 10,
+  },
+  panelSummary: {
+    backgroundColor: '#ffffff',
+  },
+  content: {
+    padding: 10,
+  },
+  contentTxt: {
+    marginBottom: 10,
+    height: 30,
+  },
+  actionTxt: {
+    color: '#aa6',
+  },
+};
+
+const title = classes => (
+  <ExpansionPanelSummary
+    expandIcon={<ExpandMoreIcon />}
+    className={classNames(classes.panelSummary)}
+  >
+    <InfoIcon />
+  </ExpansionPanelSummary>
+);
+
+const content = classes => (
+  <div className={classNames(classes.content)}>
+    <Typography className={classNames(classes.contentTxt, 'txt-panel-content')}>
+      <i className={classNames(classes.actionTxt)}>
+        Right-click
+      </i>
+      <span> to reset zoom!</span>
+    </Typography>
+    <Typography className={classNames(classes.contentTxt, 'txt-panel-content')}>
+      <i className={classNames(classes.actionTxt)}>
+        Left-click
+      </i>
+      <span> to SetShift, AddPeak or ClearPeak, base on the selection.</span>
+    </Typography>
+  </div>
+);
+
+const Manual = ({ classes }) => (
+  <ExpansionPanel>
+    { title(classes) }
+    <Divider />
+    { content(classes) }
+  </ExpansionPanel>
+);
+
+Manual.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Manual);
