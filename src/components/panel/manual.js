94236152682cfa5016e56e04184e9cfb59f00cde
jasonych99 <jasonych99@gmail.com>
2019-01-04 16:40:10 +0100

    manual icon

--- src/components/panel/manual.js
+++ src/components/panel/manual.js
@@ -3,12 +3,10 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
 import { withStyles } from '@material-ui/core/styles';
-import InfoIcon from '@material-ui/icons/Info';
 import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import Divider from '@material-ui/core/Divider';
-import Card from '@material-ui/core/Card';
 import Typography from '@material-ui/core/Typography';
 
 const styles = {
@@ -37,7 +35,9 @@ const title = classes => (
     expandIcon={<ExpandMoreIcon />}
     className={classNames(classes.panelSummary)}
   >
-    <InfoIcon />
+    <Typography className="txt-panel-header">
+      Info
+    </Typography>
   </ExpansionPanelSummary>
 );
 
