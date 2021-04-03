62a5dd61b86bd6ffb1b69456138c97b1e197097f
JasonYCHuang <jasonych99@gmail.com>
2021-04-03 15:11:24 +0800

    add carbonFeatures func

---
+++ src/helpers/carbonFeatures.js
@@ -0,0 +1,33 @@
+import { calcMpyCenter } from './multiplicity_calc';
+
+const carbonFeatures = (peaksEdit, multiplicitySt) => {
+    const { stack, shift } = multiplicitySt;
+    const nmrMpyCenters = stack.map((stk) => {
+      return {
+        x: calcMpyCenter(stk.peaks, shift, stk.mpyType),
+        y: 0,
+      };
+    });
+    let targetIdxs = [];
+    stack.forEach((stk) => { // find peak idxs to be removed
+      stk.peaks.forEach((p) => {
+        let targetIdx = -1;
+        let minDiff = 999999999;
+        peaksEdit.forEach((pe, idx) => {
+          const xDiff = Math.abs(pe.x - p.x);
+          if (xDiff < minDiff) {
+            targetIdx = idx;
+            minDiff = xDiff;
+          }
+        })
+        targetIdxs = [...targetIdxs, targetIdx];
+      })
+    });
+    let features = [...nmrMpyCenters];
+    peaksEdit.forEach((pe, idx) => {
+        if (targetIdxs.indexOf(idx) < 0) { features = [...features, pe] }
+    });
+  return features;
+};
+
+export { carbonFeatures }; // eslint-disable-line
