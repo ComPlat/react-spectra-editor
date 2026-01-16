fe9d55dd958cf8840f8dbafbb4777475f07f98df
Nicolass67 <rousseau.nicolas67@gmail.com>
2026-01-16 17:11:39 +0100

    chore: remove dist folder

--- dist/setupTests.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
-var _enzyme = _interopRequireDefault(require("enzyme"));
-var _enzymeAdapterReact = _interopRequireDefault(require("@wojtekmaj/enzyme-adapter-react-17"));
-_enzyme.default.configure({
-  adapter: new _enzymeAdapterReact.default()
-});
