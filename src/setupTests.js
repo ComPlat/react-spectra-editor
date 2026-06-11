da25c49bb4a15f6c6077f37753477a02c7448a69
Nicolass67 <rousseau.nicolas67@gmail.com>
2026-06-11 15:32:23 +0200

    chore: update dependencies in yarn.lock and improve testing workflow with frozen lockfile

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,12 +1,3 @@
-const { TextDecoder, TextEncoder } = require('util');
-
-if (typeof global.TextDecoder === 'undefined') {
-  global.TextDecoder = TextDecoder;
-}
-if (typeof global.TextEncoder === 'undefined') {
-  global.TextEncoder = TextEncoder;
-}
-
 const Enzyme = require('enzyme');
 const AdapterModule = require('@wojtekmaj/enzyme-adapter-react-17');
 const Adapter = AdapterModule.default || AdapterModule;
